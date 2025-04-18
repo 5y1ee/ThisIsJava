065a576572b2873daa1100e647e30b720e4fc97a
5y1ee <yunisy51@gmail.com>
2025-04-18 17:45:43 +0900

    Just test maybe

--- thisisjava/src/TestClass.java
+++ thisisjava/src/TestClass.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TestClass {
 
@@ -6,6 +10,9 @@ public class TestClass {
 		Integer i2 = 100;
 		
 		System.out.println(i1==i2);
+		
+		Map<String, String> m = new HashMap<>();
+		List<Object> l = new ArrayList<>();
 		
 	}
 
